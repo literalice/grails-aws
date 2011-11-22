a22424ef08e6edf014e880e3cd9a9e54fba6b21a
literalice <literalice@monochromeroad.com>
2011-11-22 10:07:02 +0900

    changing the version string

--- AwsGrailsPlugin.groovy
+++ AwsGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.aws.meta.AwsPluginSupport
 
 class AwsGrailsPlugin {
 
-    def version                  = "1.2.12.1-literalice"
+    def version                  = "1.2.12.1.p1"
     def grailsVersion            = "1.3.0 > *"
     def dependsOn                = [:]
     def loadAfter                = ['services', 'controllers']
