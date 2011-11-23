31957ea98eb9c9990cbba74b2847e4d94ac6660a
literalice <literalice@monochromeroad.com>
2011-11-24 00:47:07 +0900

    edits some dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,22 +19,14 @@ grails.project.dependency.resolution = {
 	dependencies {
         build 'org.apache.httpcomponents:httpclient:4.1.2'
         compile 'net.java.dev.jets3t:jets3t:0.8.1'
-        compile 'javax.mail:mail:1.4.1'
-        compile 'commons-httpclient:commons-httpclient:3.1'
-        compile 'commons-logging:commons-logging:1.1.1'
-		compile 'org.codehaus.jackson:jackson-core-asl:1.7.2'
 		compile 'com.amazonaws:aws-java-sdk:1.2.12', {
-			excludes 'stax-api', 'jackson-core-asl', 'commons-httpclient', 'commons-logging'
-		}
+            excludes 'stax-api'
+        }
      }
 
 	plugins {
 
-		build ":tomcat:$grailsVersion", {
-            export = false
-        }
-		
-		build  ':release:1.0.0.RC3', {
+		build(":tomcat:$grailsVersion", ':release:1.0.0.RC3') {
             export = false
         }
         
